3026043993c2a5b94a181b912a54d4872d6af366
Alvis HT Tang <alvis@hilbert.space>
2020-06-22 22:58:46 +0100

    feat: use gatsby as the site compiler

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+/*
+ *                            *** MIT LICENSE ***
+ * -------------------------------------------------------------------------
+ * This code may be modified and distributed under the MIT license.
+ * See the LICENSE file for details.
+ * -------------------------------------------------------------------------
+ *
+ * @summary   Load gatsby-plugin-ts-config to make typescript available elsewhere.
+ *
+ *            See https://www.gatsbyjs.org/docs/gatsby-config
+ *            for detailed usage
+ *
+ * @author    Alvis HT Tang <alvis@hilbert.space>
+ * @license   MIT
+ * @copyright Copyright (c) 2020 - All Rights Reserved.
+ * -------------------------------------------------------------------------
+ */
+
+module.exports = {
+  plugins: [`gatsby-plugin-ts-config`],
+};
